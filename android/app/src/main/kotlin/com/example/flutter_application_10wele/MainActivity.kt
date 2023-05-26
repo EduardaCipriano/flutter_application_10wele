fa4285ef57afa0b3a8a7a56ce3b638ce574186f7
EduardaCipriano <eduardafcipriano16@gmail.com>
2023-05-26 00:04:46 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_application_10wele/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_10wele
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'elevated.button.dart';
+
+
+void main() {
+  runApp(const MaterialApp(home:  MyButton()));
+}
